7360512accef0e97819e15011f6e645c800142fe
Yoonsung <lvev9925@naver.com>
2014-07-10 13:21:52 +0900

    Spring MVC 설정, Controller 생성 및 View 연결테스트 완료

---
+++ RbGroup/src/main/java/kr/wearit/web/IndexController.java
@@ -0,0 +1,12 @@
+package kr.wearit.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+	@RequestMapping("/")
+	public String entrance() {
+		return "index.jsp";
+	}
+}
