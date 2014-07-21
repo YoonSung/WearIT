f211028e6f1f844866540fa7f9f1ad2eba9043c2
Yoonsung <lvev9925@naver.com>
2014-07-21 12:50:49 +0900

    Refactoring Test Code

--- RbGroup/src/test/java/kr/wearit/web/UserDaoTest.java
+++ RbGroup/src/test/java/kr/wearit/web/UserDaoTest.java
@@ -1,23 +1,23 @@
 package kr.wearit.web;
 
-import javax.sql.DataSource;
-import org.apache.commons.dbcp.BasicDataSource;
-import org.junit.Test;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-import static org.hamcrest.CoreMatchers.*;
-import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertThat;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:/applicationContext.xml")
 public class UserDaoTest {
+	
+	@Autowired
+	UserDao dao;
 	
 	@Test
 	public void create() {
-		UserDao dao = new UserDao();
-		
-		ApplicationContext context = new ClassPathXmlApplicationContext("classpath:/applicationContext.xml");
-		DataSource dataSource = context.getBean("dataSource", BasicDataSource.class);
-		dao.setDataSource(dataSource);
-		
 		assertThat(dao, notNullValue());
 	}
 }
