b977ed9bf56ab35b131d25a5b8dfca1c095db18f
Yoonsung <lvev9925@naver.com>
2014-07-21 13:02:59 +0900

    데이터베이스 자동초기화 방식적용

--- RbGroup/src/main/java/kr/wearit/web/UserDao.java
+++ RbGroup/src/main/java/kr/wearit/web/UserDao.java
@@ -1,11 +1,27 @@
 package kr.wearit.web;
 
+import javax.annotation.PostConstruct;
 import javax.sql.DataSource;
 
-public class UserDao {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.core.support.JdbcDaoSupport;
+import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+
+public class UserDao extends JdbcDaoSupport{
+	
+	private static final Logger logger = LoggerFactory.getLogger(UserDao.class);
+	
 	DataSource dataSource;
 	
-	public void setDataSource(DataSource dataSource) {
-		this.dataSource = dataSource;
+	@PostConstruct
+	public void create() {
+		ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
+		populator.addScript(new ClassPathResource("user.sql"));
+		DatabasePopulatorUtils.execute(populator, getDataSource());
+		
+		logger.info("Database Initialized Success!!");
 	}
 }
