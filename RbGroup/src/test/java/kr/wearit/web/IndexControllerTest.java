799608beb95656095668ef86acd25563e02e701a
Yoonsung <lvev9925@naver.com>
2014-08-11 13:32:48 +0900

    MockMvc Extract to Variable. Prepare create test code

--- RbGroup/src/test/java/kr/wearit/web/IndexControllerTest.java
+++ RbGroup/src/test/java/kr/wearit/web/IndexControllerTest.java
@@ -4,15 +4,31 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.springframework.test.web.servlet.MockMvc;
 
 public class IndexControllerTest {
 
+	MockMvc mockMvc;
+	
+	@Before
+	public void setUp() {
+		this.mockMvc = standaloneSetup(new IndexController()).build();
+	}
+	
+	//SimpleConrollerTest 참조
 	@Test
 	public void reqeust() throws Exception {
-		standaloneSetup(new IndexController()).build()
+		mockMvc
 		.perform(get("/"))
 		.andExpect(status().isOk())
 		.andExpect(forwardedUrl("index"));
+	}
+	
+	//FormControllerTests 참조
+	@Test
+	public void create() {
+		
 	}
 }
