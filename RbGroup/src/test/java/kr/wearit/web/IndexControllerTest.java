f768fbe797de17c1dfa9401d4aa39a474fed45c3
Yoonsung <lvev9925@naver.com>
2014-08-11 13:24:13 +0900

    Spring MVC Test를 위한 클래스 추가. showcase project 를 참고로 home Request에 return status 테스트

---
+++ RbGroup/src/test/java/kr/wearit/web/IndexControllerTest.java
@@ -0,0 +1,17 @@
+package kr.wearit.web;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
+
+import org.junit.Test;
+
+public class IndexControllerTest {
+
+	@Test
+	public void reqeust() throws Exception {
+		standaloneSetup(new IndexController()).build()
+		.perform(get("/"))
+		.andExpect(status().isOk());
+	}
+}
