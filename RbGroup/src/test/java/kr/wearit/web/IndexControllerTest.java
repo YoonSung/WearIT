151656557bb96c9410fa2bc1b89a8a0a0258973e
Yoonsung <lvev9925@naver.com>
2014-08-11 13:43:31 +0900

    validation에 대한 showcase 코드참고해서 create 메서드 테스트

--- RbGroup/src/test/java/kr/wearit/web/IndexControllerTest.java
+++ RbGroup/src/test/java/kr/wearit/web/IndexControllerTest.java
@@ -1,34 +1,20 @@
 package kr.wearit.web;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.forwardedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
-import org.junit.Before;
 import org.junit.Test;
-import org.springframework.test.web.servlet.MockMvc;
 
 public class IndexControllerTest {
-
-	MockMvc mockMvc;
-	
-	@Before
-	public void setUp() {
-		this.mockMvc = standaloneSetup(new IndexController()).build();
-	}
-	
+		
 	//SimpleConrollerTest 참조
 	@Test
 	public void reqeust() throws Exception {
-		mockMvc
+		standaloneSetup(new IndexController()).build()		
 		.perform(get("/"))
 		.andExpect(status().isOk())
 		.andExpect(forwardedUrl("index"));
-	}
-	
-	//FormControllerTests 참조
-	@Test
-	public void create() {
-		
 	}
 }
---
+++ RbGroup/src/test/java/kr/wearit/web/UserControllerTest.java
@@ -0,0 +1,30 @@
+package kr.wearit.web;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.test.web.servlet.MockMvc;
+
+public class UserControllerTest {
+
+	MockMvc mockMvc;
+
+	@Before
+	public void setUp() {
+		this.mockMvc = standaloneSetup(new IndexController()).build();
+	}
+	
+	// FormControllerTests 참조
+	@Test
+	public void create() throws Exception {
+		mockMvc.perform(post("/user/register")
+				.param("userId", "lvev9925")
+				.param("password", "Yoonsung")
+				.param("name", "JungYoonSung")
+				.param("email",""))
+		.andDo(print());
+	}
+}
