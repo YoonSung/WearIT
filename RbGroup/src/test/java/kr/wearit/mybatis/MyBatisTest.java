da56b8ec5ea2a8eaae3f19e33d15ca456186a277
Yoonsung <lvev9925@naver.com>
2014-09-15 09:32:29 +0900

    test-config 파일로 이름 재설정, MyBatis findById Test 완료

--- RbGroup/src/test/java/kr/wearit/mybatis/MyBatisTest.java
+++ RbGroup/src/test/java/kr/wearit/mybatis/MyBatisTest.java
@@ -1,22 +1,44 @@
 package kr.wearit.mybatis;
 
+import static org.junit.Assert.assertNotNull;
+
 import java.io.InputStream;
 
+import kr.wearit.domain.User;
+
 import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
-import static org.hamcrest.CoreMatchers.*;
-import static org.junit.Assert.*;
-
 public class MyBatisTest {
-	@Test
-	public void gettingStart() throws Exception {
-		String resource = "mybatis-config.xml";
+	
+	SqlSession session;
+
+	@Before
+	public void setUp() throws Exception {
+		String resource = "mybatis-test-config.xml";
 		InputStream inputStream = Resources.getResourceAsStream(resource);
 		SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
-
+		session = sqlSessionFactory.openSession(); 
 		assertNotNull(sqlSessionFactory);
+	}
+	
+	@After
+	public void after() throws Exception {
+		session.close();
+	}
+	
+	@Test
+	public void findById() throws Exception {
+		User user = new User("Yoonsung", null, null, null);
+		User result = session.selectOne("kr.wearit.user.UserMapper.findById", user);
+		//User result = session.selectOne("kr.wearit.user.UserMapper.findById", "Yoonsung");
+		
+		System.out.println(result.toString());
+		assertNotNull(result.getEmail());
 	}
 }
